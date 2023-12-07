7f4babc03f2c3032a2dc54c426b46d9efd684c2f
Liweidong <357879926@qq.com>
2023-12-07 12:03:34 +0800

    get-vaults path error

--- src/wallet-and-address/get-vaults.js
+++ src/wallet-and-address/get-vaults.js
@@ -3,7 +3,7 @@ import { ApiBase } from '../api-base.js';
 export class GetVaults extends ApiBase {
     constructor(url, privKey, pubKey) {
         super({
-            target: '/v1/waas/mpc/wallet/get_vaults',
+            target: '/v1/waas/common/get_vaults',
             method: 'post',
             url,
             privKey,
